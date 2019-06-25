40ad42120b527076c3fde30dbe92ba4b96251078
Stephen Lee <sungsoolee0127@gmail.com>
2019-06-24 21:56:19 -0700

    init

---
+++ stanford-assignment-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stanford-assignment-2
+//
+//  Created by Stephen lee on 6/24/19.
+//  Copyright © 2019 Stephen lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
